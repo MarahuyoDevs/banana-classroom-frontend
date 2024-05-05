4b76d1b0562c3ebadc430802dfe8f5f65f3802a5
karlrobeck <karlalferezfx@gmail.com>
2024-05-05 21:09:56 +0800

    add quiz logic

--- src/routes/quiz/start/[id]/+page.server.ts
+++ src/routes/quiz/start/[id]/+page.server.ts
@@ -1,10 +1,25 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { readQuizByID } from "$lib/database/crud/quiz";
 
 export const load: PageServerLoad = async ({ params }) => {
     // get the quiz from path
     const quiz = await readQuizByID(params.id)
-    console.log(quiz)
-    console.log(quiz.questions.L)
     return { quiz }
+}
+
+export const actions: Actions = {
+    default: async (event) => {
+        const quiz = await readQuizByID(event.params.id)
+
+        let score = 0;
+
+        for (let [index, answer] of (await event.request.formData()).entries()) {
+            let question = quiz.questions.L?.at(parseInt(index) - 1);
+            if (question.M.answer.S === answer) {
+                score += 1;
+            }
+        }
+        // save this as a quiz result.
+        console.log(score);
+    }
 }
--- src/routes/quiz/start/[id]/+page.svelte
+++ src/routes/quiz/start/[id]/+page.svelte
@@ -3,6 +3,7 @@
 	import * as Card from '$lib/components/ui/card';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import * as RadioGroup from '$lib/components/ui/radio-group/index.js';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { Label } from '$lib/components/ui/label/index.js';
 	export let data: PageData;
 </script>
@@ -12,30 +13,37 @@
 	<meta name="description" content={data.quiz.description.S} />
 </svelte:head>
 
-<h1>{data.quiz.name.S}</h1>
-<p>{data.quiz.description.S}</p>
-
-{#each data.quiz.questions.L as question}
-	<Card.Root>
-		<Card.Header>
-			<Card.Title>Question {question.M.index.S}</Card.Title>
-			<Card.Description>{question.M.text.S}</Card.Description>
-		</Card.Header>
-		{#if question.M.type.S === 'multipleChoice'}
-			<Card.Content>
-				<RadioGroup.Root value="comfortable">
-					{#each question.M.options.L as choice}
-						<div class="flex items-center space-x-2">
-							<RadioGroup.Item value={choice.S} />
-							<Label for="r1">{choice.S}</Label>
-						</div>
-					{/each}
-				</RadioGroup.Root>
-			</Card.Content>
-		{:else if question.M.type.S === 'identification'}
-        <Card.Footer>
-            <Input placeholder="enter your answer" />
-        </Card.Footer>
-        {/if}
-	</Card.Root>
-{/each}
+<h4>{data.quiz.name.S}</h4>
+<p class="muted">{data.quiz.description.S}</p>
+<form action="" method="POST">
+	<div class="space-y-5 py-4">
+		{#each data.quiz.questions.L as question}
+			<Card.Root>
+				<Card.Header>
+					<Card.Title>Question {question.M.index.S}</Card.Title>
+					<Card.Description>{question.M.text.S}</Card.Description>
+				</Card.Header>
+				{#if question.M.type.S === 'multipleChoice'}
+					<Card.Content>
+						<RadioGroup.Root value="">
+							{#each question.M.options.L as choice}
+								<div class="flex items-center space-x-2">
+									<RadioGroup.Item value={choice.S} />
+									<Label for="r1">{choice.S}</Label>
+								</div>
+							{/each}
+							<RadioGroup.Input name={question.M.index.S} />
+						</RadioGroup.Root>
+					</Card.Content>
+				{:else if question.M.type.S === 'identification'}
+					<Card.Footer>
+						<Input name={question.M.index.S} placeholder="enter your answer" />
+					</Card.Footer>
+				{/if}
+			</Card.Root>
+		{/each}
+	</div>
+	<div>
+		<Button type="submit" variant="default">Submit</Button>
+	</div>
+</form>
